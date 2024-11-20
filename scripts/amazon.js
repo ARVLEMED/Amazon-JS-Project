1c073ea8f16407cadf6ddc3213253b40448a168e
aron k koech <varokoaruna81@gmail.com>
2024-11-20 16:03:42 +0300

    load products from backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,11 @@
 import {cart, addToCart} from '../data/cart.js'
-import { products} from '../data/products.js';
+import { products, loadProducts} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+
+loadProducts (renderProductsGrid);
+
+function renderProductsGrid(){
 let productsHTML='';
 products.forEach((product)=>{
     productsHTML +=`<div class="product-container">
@@ -78,3 +82,4 @@ function updateCartQuantity(){
 
 });
 
+}
