378aac2c064cd44d6efa9f454d5b4ddb18610c1c
aron k koech <varokoaruna81@gmail.com>
2024-11-20 16:56:36 +0300

    converted code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,8 +3,20 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
 //import '../data/cart-class.js';
+
+new Promise((resolve) => {
+    loadProducts(()=>{
+        resolve();
+    });
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+/*
 loadProducts(()=>{
 
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
